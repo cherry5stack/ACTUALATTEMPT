7d93d88b13d71d7c8b87c0bc787dc469a9779e6b
cherry5stack <cherryist5@gmail.com>
2026-06-15 02:59:44 -0400

    Update AttacksData.lua

--- Place1/src/ReplicatedStorage/AttacksData.lua
+++ Place1/src/ReplicatedStorage/AttacksData.lua
@@ -1,23 +1,31 @@
 local AttacksData = {}
 
+--default debounce is 1 second
 AttacksData["Punch"] = {
 	Damage = 10,
 	Cooldown = 1,
+	AttackDebounce = 1, --time to no be able to trigger another attack after casting this attack
 	Priority = 1,
-	AnimationId = "rbxassetid://YOUR_ID_HERE",
+	AnimationName = "Punch",   -- matches Animation object name in RS.Animations
 	HitboxSize = Vector3.new(4, 4, 4),
-	HitboxOffset = 1, -- how far forward from NPC center
-	Range = 5, -- how close enemy needs to be to use this attack
+	HitboxOffset = 1,
+	Range = 5,
+	AttackSpeed = 1,
 }
 
 AttacksData["HeavySlam"] = {
-	Damage = 25,
-	Cooldown = 3,
-	AnimationId = "rbxassetid://YOUR_ID_HERE",
+	Damage = 100,
+	PerTick = 0.5,
+	Cooldown = 13,
+	AnimationName = "Kick",
 	HitboxSize = Vector3.new(8, 4, 8),
 	HitboxOffset = 2,
 	Range = 6,
 	Priority = 2,
+	HitboxDuration = 3,
+	StaticHitbox = true,
+	ParticleEffect = "SlamEffect",  -- name of ParticleEmitter in RS.ParticleEffects
+	AttackSpeed = 0.5,
 }
 
 return AttacksData
